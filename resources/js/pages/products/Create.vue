a23040ef722c20558081ee211744d8b239bbc4b2
amirhamzan <amirhamzan30@gmail.com>
2026-04-19 16:44:29 +0800

    Move category option section below product name at Create

--- resources/js/pages/products/Create.vue
+++ resources/js/pages/products/Create.vue
@@ -42,6 +42,15 @@ const categories = computed(() => page.props.categories);
                 <form @submit.prevent="form.post(route('products.store'))">
                     <div class="border-b border-gray-900/10 mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div class="sm:col-span-3">
+                            <label for="name" class="block text-sm/6 font-medium text-gray-900">Product Name</label>
+                            <div class="mt-2">
+                                <input required id="name" type="text" v-model="form.name" autocomplete="off"
+                                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="border-b border-gray-900/10 mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div class="sm:col-span-3">
                             <label for="category_id" class="block text-sm/6 font-medium text-gray-900">Category
                                 Name</label>
                             <div class="mt-2">
@@ -51,15 +60,6 @@ const categories = computed(() => page.props.categories);
                                         {{ category.name }}
                                     </option>
                                 </select>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="border-b border-gray-900/10 mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div class="sm:col-span-3">
-                            <label for="name" class="block text-sm/6 font-medium text-gray-900">Product Name</label>
-                            <div class="mt-2">
-                                <input required id="name" type="text" v-model="form.name" autocomplete="off"
-                                    class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                             </div>
                         </div>
                     </div>
